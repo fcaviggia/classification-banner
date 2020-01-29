fcaviggia/classification-banner

commit 09deeaed94536d6cc565ed03ffe0f83bdadb06aa

Replace optparse with argparse

classification_banner/banner.py
@@ -4,7 +4,7 @@
 
 import sys
 import os
-import optparse
+import argparse
 import time
 from ConfigParser import SafeConfigParser
 from socket import gethostname
@@ -221,7 +221,7 @@ class Display_Banner:
             pass
 
         # Launch Banner
-        self.config, self.args = self.configure()
+        self.config = self.configure()
         self.execute(self.config)
 
     # Read Global configuration
@@ -248,43 +248,43 @@ class Display_Banner:
             defaults[key] = val
 
         # Use the global config to set defaults for command line options
-        parser = optparse.OptionParser()
-        parser.add_option("-m", "--message", default=defaults["message"],
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-m", "--message", default=defaults["message"],
                           help="Set the Classification message")
-        parser.add_option("-f", "--fgcolor", default=defaults["foreground"],
+        parser.add_argument("-f", "--fgcolor", default=defaults["foreground"],
                           help="Set the Foreground (text) color")
-        parser.add_option("-b", "--bgcolor", default=defaults["background"],
+        parser.add_argument("-b", "--bgcolor", default=defaults["background"],
                           help="Set the Background color")
-        parser.add_option("-x", "--hres", default=defaults["horizontal_resolution"], type="int",
+        parser.add_argument("-x", "--hres", default=defaults["horizontal_resolution"], type=int,
                           help="Set the Horizontal Screen Resolution")
-        parser.add_option("-y", "--vres", default=defaults["vertical_resolution"], type="int",
+        parser.add_argument("-y", "--vres", default=defaults["vertical_resolution"], type=int,
                           help="Set the Vertical Screen Resolution")
-        parser.add_option("-o", "--opacity", default=defaults["opacity"],
-                          type="float", dest="opacity",
+        parser.add_argument("-o", "--opacity", default=defaults["opacity"],
+                          type=float, dest="opacity",
                           help="Set the window opacity for composted window managers")
-        parser.add_option("--font", default=defaults["font"], help="Font type")
-        parser.add_option("--size", default=defaults["size"], help="Font size")
-        parser.add_option("--weight", default=defaults["weight"],
+        parser.add_argument("--font", default=defaults["font"], help="Font type")
+        parser.add_argument("--size", default=defaults["size"], help="Font size")
+        parser.add_argument("--weight", default=defaults["weight"],
                           help="Set the Font weight")
-        parser.add_option("--disable-esc", default=defaults["esc"],
+        parser.add_argument("--disable-esc", default=defaults["esc"],
                           dest="esc", action="store_false",
                           help="Disable the 'ESC to hide' message")
-        parser.add_option("--hide-top", default=defaults["show_top"],
+        parser.add_argument("--hide-top", default=defaults["show_top"],
                           dest="show_top", action="store_false",
                           help="Disable the top banner")
-        parser.add_option("--hide-bottom", default=defaults["show_bottom"],
+        parser.add_argument("--hide-bottom", default=defaults["show_bottom"],
                           dest="show_bottom", action="store_false",
                           help="Disable the bottom banner")
-        parser.add_option("--system-info", default=defaults["sys_info"],
+        parser.add_argument("--system-info", default=defaults["sys_info"],
                           dest="sys_info", action="store_true",
                           help="Show user and hostname in the top banner")
-        parser.add_option("--enable-spanning", default=defaults["spanning"],
+        parser.add_argument("--enable-spanning", default=defaults["spanning"],
                           dest="spanning", action="store_true",
                           help="Enable banner(s) to span across screens as a single banner")
 
-        options, args = parser.parse_args()
+        args = parser.parse_args()
 
-        return options, args
+        return args
 
     # Launch the Classification Banner Window(s)
     def execute(self, options):

commit 5322c73026a03db1248924f9334fc819471d80e7

Fix a regression introduced with commit 09deeae ('Replace optparse with argparse'); the method call was updated on line 224 for __init__, but wasn't applied to resize

classification_banner/banner.py
@@ -428,7 +428,7 @@ class DislayBanner:
 
     # Relaunch the Classification Banner on Screen Resize
     def resize(self, widget, data=None):
-        self.config, self.args = self.configure()
+        self.config = self.configure()
         self.execute(self.config)
         return True
 